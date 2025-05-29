23c89703aedd3d9635b0268a251e65cd24e9453e
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-05-29 17:59:06 +0100

    update

--- crud-spring/src/main/java/com/crudspring/crud_spring/service/userService.java
+++ crud-spring/src/main/java/com/crudspring/crud_spring/service/userService.java
@@ -1,4 +1,7 @@
 package com.crudspring.crud_spring.service;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import com.crudspring.crud_spring.entity.user;
 import com.crudspring.crud_spring.repository.userRep;
@@ -11,6 +14,12 @@ public class userService {
 
     public user SaveUser(user user){
         return userResp.save(user);
+    }
+    public List<user> findAll() {
+        return userResp.findAll();
+    }
+    public Optional<user> findById(String id) {
+        return userResp.findById(id);
     }
 
 }
